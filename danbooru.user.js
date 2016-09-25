440ddc577414405c5543fa5e83e6bff5c63ba306
evazion <noizave@gmail.com>
2016-09-25 16:11:49 -0500

    352: New unstable release.

--- danbooru.user.js
+++ danbooru.user.js
@@ -1,14 +1,14 @@
 // ==UserScript==
 // @name         Danbooru EX
 // @namespace    https://github.com/evazion/danbooru-ex
-// @version      351
+// @version      352
 // @source       https://danbooru.donmai.us/users/52664
 // @description  Danbooru UI Enhancements
 // @author       evazion
 // @match        *://*.donmai.us/*
 // @grant        none
-// @updateURL    https://github.com/evazion/danbooru-ex/raw/stable/danbooru.user.js
-// @downloadURL  https://github.com/evazion/danbooru-ex/raw/stable/danbooru.user.js
+// @updateURL    https://github.com/evazion/danbooru-ex/raw/master/danbooru.user.js
+// @downloadURL  https://github.com/evazion/danbooru-ex/raw/master/danbooru.user.js
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/selectable.js
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/tooltip.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.js
