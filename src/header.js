0cda05764a8cb4c54ea774ef1e74c88ac8164a91
evazion <noizave@gmail.com>
2016-11-02 23:35:14 -0500

    [fix] Set updateURL to stable branch.

--- src/header.js
+++ src/header.js
@@ -9,20 +9,14 @@
 // @match        *://localhost/*
 // @grant        none
 // @run-at       document-body
-// @updateURL    https://github.com/evazion/danbooru-ex/raw/master/dist/danbooru-ex.user.js
-// @downloadURL  https://github.com/evazion/danbooru-ex/raw/master/dist/danbooru-ex.user.js
+// @updateURL    https://github.com/evazion/danbooru-ex/raw/stable/dist/danbooru-ex.user.js
+// @downloadURL  https://github.com/evazion/danbooru-ex/raw/stable/dist/danbooru-ex.user.js
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/selectable.js
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/tooltip.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/dexie/1.5.0/dexie.min.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.15.0/lodash.js
 // @require      https://unpkg.com/filesize@3.3.0
 // ==/UserScript==
-
-// @updateURL    http://127.0.0.1:8000/danbooru-ex.user.js
-// @downloadURL  http://127.0.0.1:8000/danbooru-ex.user.js
-// @updateURL    https://github.com/evazion/danbooru-ex/raw/master/danbooru-ex.user.js
-// @downloadURL  https://github.com/evazion/danbooru-ex/raw/master/danbooru-ex.user.js
 
 /*
  * What is a userscript? A miserable pile of hacks.
