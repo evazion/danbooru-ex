530d81f6412eab5ff4a1fd8f935642e6005bf681
evazion <noizave@gmail.com>
2016-11-03 03:50:17 -0500

    Release 1301.

--- src/header.js
+++ src/header.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Danbooru EX
 // @namespace    https://github.com/evazion/danbooru-ex
-// @version      1300
+// @version      1301
 // @source       https://danbooru.donmai.us/users/52664
 // @description  Danbooru UI Enhancements
 // @author       evazion
