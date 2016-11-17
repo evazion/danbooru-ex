76627a2d5ff88c378bf84f6c25be11cd38d92474
evazion <noizave@gmail.com>
2016-11-16 19:28:05 -0600

    [refactor] Reorder header.js.

--- src/ui/header.js
+++ src/ui/header.js
@@ -1,3 +1,6 @@
+import $ from "jquery";
+import _ from "lodash";
+
 import ModeMenu from "./mode_menu.js";
 import PreviewPanel from "./preview_panel.js";
 
@@ -16,7 +19,65 @@ export default class Header {
   }
 
   static initializeHeader() {
-    let $header = $(`
+    let $header = $(Header.render()).insertBefore("#top");
+    _.defer(() => $header.show());
+
+    // Initalize header search box.
+    $("#ex-header #tags").val($("#sidebar #tags").val());
+    Danbooru.Autocomplete.initialize_all();
+
+    $(".ex-header-close").click(Header.toggleClose);
+
+    $(document).scroll(_.throttle(Header.onScroll, 16));
+  }
+
+  static initializeModeMenu() {
+    $(".ex-mode-menu").show();
+    ModeMenu.initialize();
+  }
+
+  static initializeHotkeys() {
+    let $search = $("#ex-header #tags");
+
+    $search.keydown("ctrl+return", e => {
+      const tags = $(e.target).val().trim();
+      window.open(`/posts?tags=${encodeURIComponent(tags)}`, "_blank").focus();
+    });
+
+    // Shift+Q: Focus and search box.
+    $(document).keydown('shift+q', e => {
+      // Add a space to end if box is non-empty and doesn't already have trailing space.
+      $search.val().length && $search.val((i, v) => v.replace(/\s*$/, ' '));
+      $search.focus();
+
+      e.preventDefault();
+    });
+  }
+
+  static onScroll() {
+    $("#ex-header").toggleClass("ex-header-scrolled", window.scrollY > 0, { duration: 100 });
+    // XXX Shrink header after scrolling past navbar.
+    // $("header h1").toggleClass("ex-small-header", window.scrollY > 0, { duration: 100 });
+  }
+
+  static toggleClose(event) {
+    let $header = $("#ex-header");
+
+    if ($header.hasClass("ex-fixed")) {
+      $header.slideUp().promise().then(e => {
+        $header.toggleClass("ex-fixed ex-static").show();
+        EX.config.headerState = $header.attr("class");
+      });
+    } else {
+      $header.toggleClass("ex-fixed ex-static");
+      EX.config.headerState = $header.attr("class");
+    }
+
+    event.preventDefault();
+  }
+
+  static render() {
+    return `
       <header style="display: none;" id="ex-header" class="${EX.config.headerState}">
         <h1><a href="/">Danbooru</a></h1>
 
@@ -59,60 +120,6 @@ export default class Header {
           <i class="fa fa-lg" aria-hidden="true"></i>
 	</span>
       </header>
-    `).insertBefore("#top");
-    _.defer(() => $header.show());
-
-    // Initalize header search box.
-    $("#ex-header #tags").val($("#sidebar #tags").val());
-    Danbooru.Autocomplete.initialize_all();
-
-    $(".ex-header-close").click(Header.toggleClose);
-
-    $(document).scroll(_.throttle(Header.onScroll, 16));
-  }
-
-  static onScroll() {
-    $("#ex-header").toggleClass("ex-header-scrolled", window.scrollY > 0, { duration: 100 });
-    // XXX Shrink header after scrolling past navbar.
-    // $("header h1").toggleClass("ex-small-header", window.scrollY > 0, { duration: 100 });
-  }
-
-  static initializeHotkeys() {
-    let $search = $("#ex-header #tags");
-
-    $search.keydown("ctrl+return", e => {
-      const tags = $(e.target).val().trim();
-      window.open(`/posts?tags=${encodeURIComponent(tags)}`, "_blank").focus();
-    });
-
-    // Shift+Q: Focus and search box.
-    $(document).keydown('shift+q', e => {
-      // Add a space to end if box is non-empty and doesn't already have trailing space.
-      $search.val().length && $search.val((i, v) => v.replace(/\s*$/, ' '));
-      $search.focus();
-
-      e.preventDefault();
-    });
-  }
-
-  static initializeModeMenu() {
-    $(".ex-mode-menu").show();
-    ModeMenu.initialize();
-  }
-
-  static toggleClose(event) {
-    let $header = $("#ex-header");
-
-    if ($header.hasClass("ex-fixed")) {
-      $header.slideUp().promise().then(e => {
-        $header.toggleClass("ex-fixed ex-static").show();
-        EX.config.headerState = $header.attr("class");
-      });
-    } else {
-      $header.toggleClass("ex-fixed ex-static");
-      EX.config.headerState = $header.attr("class");
-    }
-
-    event.preventDefault();
+    `;
   }
 }
