c2480186fb8d6203750af0718f9f3c59270ffaed
evazion <noizave@gmail.com>
2017-11-10 13:20:27 -0600

    [chore] Update lodash to 4.17.4

--- src/header.js
+++ src/header.js
@@ -13,7 +13,7 @@
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/selectable.js
 // @require      https://raw.githubusercontent.com/jquery/jquery-ui/1.11.2/ui/tooltip.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/moment.js/2.14.1/moment.js
-// @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.15.0/lodash.js
+// @require      https://cdnjs.cloudflare.com/ajax/libs/lodash.js/4.17.4/lodash.js
 // @require      https://unpkg.com/filesize@3.3.0
 // @require      https://unpkg.com/css-element-queries@0.3.2/src/ResizeSensor.js
 // @require      https://cdnjs.cloudflare.com/ajax/libs/qtip2/3.0.3/jquery.qtip.js
